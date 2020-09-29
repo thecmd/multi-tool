8bbb0f1ec4c1b38a10d3e65fc2d6ab9262289976
thecmd <71965479+thecmd@users.noreply.github.com>
2020-09-29 19:57:36 +0100

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,53 @@
+echo "███████╗██╗  ██╗███╗   ██╗███████╗██╗   ██╗    ";
+echo "╚══███╔╝██║  ██║████╗  ██║╚══███╔╝██║   ██║    ";
+echo "  ███╔╝ ███████║██╔██╗ ██║  ███╔╝ ██║   ██║    ";
+echo " ███╔╝  ╚════██║██║╚██╗██║ ███╔╝  ██║   ██║    ";
+echo "███████╗     ██║██║ ╚████║███████╗╚██████╔╝    ";
+echo "╚══════╝     ╚═╝╚═╝  ╚═══╝╚══════╝ ╚═════╝     ";
+echo "                                               ";
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/multi-tool/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/multi-tool
+
+clear
+
+mkdir multi-tool
+
+clear
+
+cd multi-tool
+
+clear
+
+git clone https://github.com/thecmd/multi-tool.git
+
+clear
+
+cd multi-tool
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
